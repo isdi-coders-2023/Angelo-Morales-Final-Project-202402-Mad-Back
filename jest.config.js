afcbba5f2b8c418943548cfea842584626b63a09
angelo morales <angelotuba3@gmail.com>
2024-04-30 17:29:15 +0200

    audipass

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
 const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
